766cb11891901a141381f469c290b8db3f5c34e6
shbb <54426110+bsshhhh@users.noreply.github.com>
2024-03-15 16:59:20 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -48,7 +48,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     source.textContent = sentences[randomIndex].title + ' (' + sentences[randomIndex].year + ')' + ', ' + sentences[randomIndex].artist;
                     writer.textContent = sentences[randomIndex].writer;
                     exampleSentenceWords = sentences[randomIndex].sentence.split(/\s+/);
-                    userInput.textContent = '';
+                    // userInput.textContent = '';
+                    userInput.empty();
                     startTime = null;
                     wordCount = 0;
                     correctWordCount = 0;
